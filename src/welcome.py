903001e0a0d331e8f6a70b185de3b57e58382072
Kaan <kaan.sacma@epitech.eu>
2022-05-24 21:30:42 +0200

    [+] Update Welcome Message

--- src/welcome.py
+++ src/welcome.py
@@ -1,4 +1,7 @@
 def welcome_the_user():
     print("/----------------------------------------------------------------\\")
     print("|          -°- Welcome to Channel Video Downloader -°-           |")
+    print("|                                                                |")
+    print("|                                                    Version 0.5 |")
+    print("|                                             Made by Kaan Saçma |")
     print("\----------------------------------------------------------------/\n")
